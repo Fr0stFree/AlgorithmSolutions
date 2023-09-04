de7f38c33352775e122768470808d9a042b5453a
fr0stfree <me@frostfree.ru>
2023-09-04 21:28:30 +0300

    add some sql

---
+++ postgres/create_genre_table.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS content.film_work (
+    id uuid PRIMARY KEY,
+    title TEXT NOT NULL,
+    description TEXT,
+    creation_date DATE,
+    rating FLOAT,
+    type TEXT not null,
+    created timestamp with time zone,
+    modified timestamp with time zone
+);
--- python/rabbitmq/producer.py
+++ python/rabbitmq/producer.py
@@ -1,9 +1,10 @@
 import pika
 
-CONNECTION_STRING = "amqp://guest:guest@localhost:5672/"
-QUEUE_NAME = "test_queue"
-EXCHANGE_NAME = "test_exchange"
-ROUTING_KEY = "test_routing_key"
+CONNECTION_STRING = "amqps://student:XYR4yqc.cxh4zug6vje@rabbitmq-exam.rmq3.cloudamqp.com/mxifnklj"
+QUEUE_NAME = "exam"
+MESSAGE = "Hi CloudAMQP, this was fun!"
+ROUTING_KEY = "efa5b7f6-353f-4246-a32f-e7b6161449e4"
+EXCHANGE_NAME = "exchange.efa5b7f6-353f-4246-a32f-e7b6161449e4"
 
 
 if __name__ == "__main__":
@@ -11,19 +12,23 @@ if __name__ == "__main__":
     connection = pika.BlockingConnection(params)
     channel = connection.channel()
 
-    channel.exchange_declare(exchange=EXCHANGE_NAME, exchange_type="direct")
-    channel.queue_declare(queue=QUEUE_NAME)
-    channel.queue_bind(
+    channel.exchange_declare(
         exchange=EXCHANGE_NAME,
-        queue=QUEUE_NAME,
-        routing_key=ROUTING_KEY,
+        exchange_type="direct",
+        passive=False,
     )
+
+    channel.queue_declare(queue=QUEUE_NAME, durable=True)
+    channel.queue_bind(queue=QUEUE_NAME, exchange=EXCHANGE_NAME, routing_key=ROUTING_KEY)
 
     channel.basic_publish(
         exchange=EXCHANGE_NAME,
         routing_key=ROUTING_KEY,
-        body=b"Hello World!",
+        body=MESSAGE,
+        properties=pika.BasicProperties(delivery_mode=2),
     )
 
+    print("Message sent")
     channel.close()
     connection.close()
+
