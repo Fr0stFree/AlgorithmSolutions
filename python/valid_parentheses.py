7bb2d0855391bc8b30c082faad6f1d9e9e887f07
Krasov <me@frostfree.ru>
2024-03-27 23:32:38 +0300

    solve one problem with python

---
+++ python/valid_parentheses.py
@@ -0,0 +1,34 @@
+"""
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+"""
+from typing import Literal
+
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        for element in s:
+            if element in ("(", "{", "["):
+                stack.append(element)
+                continue
+
+            try:
+                previous = stack.pop()
+            except IndexError:
+                return False
+
+            if not any([
+                previous == "{" and element == "}",
+                previous == "[" and element == "]",
+                previous == "(" and element == ")",
+            ]):
+                return False
+
+        return not stack
+
+
+if __name__ == '__main__':
+    s = Solution()
+
+    assert s.isValid("()") is True
+    assert s.isValid(")(") is False
