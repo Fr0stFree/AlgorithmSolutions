b5691715530712d518369821da437c1459dede0a
Krasov <me@frostfree.ru>
2024-05-12 21:37:12 +0300

    solve 1 more ts problem

---
+++ typescript/isAnagram.ts
@@ -0,0 +1,26 @@
+function isAnagram(s: string, t: string): boolean {
+    if (s.length !== t.length) return false;
+
+    const map = new Map<string, number>()
+
+    for (let letter of s.split("")) {
+        const amount = map.get(letter);
+        amount !== undefined ? map.set(letter, amount + 1) : map.set(letter, 1)
+    }
+    for (let letter of t.split("")) {
+        const amount = map.get(letter);
+        amount !== undefined ? map.set(letter, amount - 1) : map.set(letter, -1)
+    }
+    return Array.from(map.entries()).every(([_, amount]) => amount === 0);
+}
+
+let result: boolean
+let expectedResult: boolean
+
+result = isAnagram("anagram", "nagaram");
+expectedResult = true;
+console.log(result === expectedResult);
+//
+result = isAnagram("rat", "car");
+expectedResult = false;
+console.log(result === expectedResult);
--- typescript/wordPattern.ts
+++ typescript/wordPattern.ts
@@ -1,26 +1,25 @@
 function wordPattern(pattern: string, s: string): boolean {
+    const words = s.split(" ");
+    const tokens = pattern.split("");
+
+    if (words.length !== pattern.length) return false;
+    if (new Set(words).size !== new Set(tokens).size) return false;
+
     const tokenToWord = new Map<string, string>();
     const wordToToken = new Map<string, string>();
-    const words = s.split(" ")
 
-    if (words.length !== pattern.length) {
-        return false
-    }
-
-    for (let [index , token] of pattern.split("").entries()) {
+    for (let [index , token] of tokens.entries()) {
         const mappedWord = tokenToWord.get(token);
         const actualWord = words[index];
-        if (mappedWord) {
-            if (mappedWord !== actualWord) {
-                return false
-            }
-        } else {
-            if (wordToToken.get(actualWord)) {
-                return false
-            }
+        if (!mappedWord) {
+            if (wordToToken.has(actualWord)) return false;
+
             tokenToWord.set(token, actualWord);
             wordToToken.set(actualWord, token);
+            continue
         }
+
+        if (mappedWord !== actualWord) return false;
     }
     return true;
 }
