70e34bce4dd2d69ba3c7315b35caf91edda90100
Krasov <me@frostfree.ru>
2024-06-02 20:45:55 +0300

    fix one problem

--- python/flatten_list.py
+++ python/flatten_list.py
@@ -1,12 +1,20 @@
 def flatten(data: list[int | list]) -> list[int]:
     result = []
+    todo = []
 
     for element in data:
-        if isinstance(element, int):
-            result.append(element)
-        else:
-            data.extend(element)
+        todo.append(element)
+
+        while todo:
+            current = todo.pop()
+
+            if isinstance(current, int):
+                result.append(current)
+            else:
+                for item in reversed(current):
+                    todo.append(item)
 
     return result
+
 
 assert flatten([3, 1, [7, [9, 1]], 4, [7], [[9, [7]], 1, 2], 3, [3, 5, 1, 6]]) == [3, 1, 7, 9, 1, 4, 7, 9, 7, 1, 2, 3, 3, 5, 1, 6]
