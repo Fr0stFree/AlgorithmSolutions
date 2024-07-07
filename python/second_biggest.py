5bc8a36c14c9b0f7a0e6c801b81d04c7df96a0b4
Krasov <me@frostfree.ru>
2024-07-07 11:07:29 +0300

    fix "second-biggest" solution

--- python/second_biggest.py
+++ python/second_biggest.py
@@ -2,15 +2,14 @@
 # При решении задачи учитывайте, что среди чисел, подающихся на вход программе, могут быть повторяющиеся.
 
 
-def solve(data: list[int]) -> int:
+def solve(numbers: list[int]) -> int:
     biggest, second = -1, -1
 
-    def update_biggest(number: int) -> None:
-        nonlocal biggest, second
-        second = biggest
-        biggest = number
-
-    [update_biggest(element) for element in data if element > biggest]
+    for number in filter(lambda num: num > second, numbers):
+        if number > biggest:
+            biggest, second = number, biggest
+        elif biggest > number > second:
+            second = number
 
     return second
 
@@ -19,6 +18,11 @@ def read_data() -> list[int]:
     _ = input()
     return list(map(int, input().strip().split(" ")))
 
+
+assert solve([4, 3, 5, 2, 7, 1]) == 5
+assert solve([4, 7, 4, 2, 7, 1]) == 4
+assert solve([5, 4, 3, 2, 1]) == 4
+assert solve([1, 2, 3, 4, 5]) == 4
 
 if __name__ == '__main__':
     data = read_data()
