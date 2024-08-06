f47b89a5c16fe58053586de77becbd683fcc1dcc
Krasov <me@frostfree.ru>
2024-08-06 21:36:11 +0300

    solve one more python problem

--- python/chain_sum.py
+++ python/chain_sum.py
@@ -10,5 +10,20 @@ def chain_sum(number: int | None = None, accumulated: int = 0) -> Callable | int
     return partial(chain_sum, accumulated=accumulated)
 
 
+def chain_sum_2(result: int | None = None) -> Callable | int:
+
+    def inner(number: int | None = None):
+        nonlocal result
+        if number is None:
+            return result or 0
+
+        result += number
+        return chain_sum_2(result)
+
+    return inner() if result is None else inner
+
+
 assert chain_sum(50)(40)(30)() == 120
 assert chain_sum() == 0
+assert chain_sum_2(50)(40)(30)() == 120
+assert chain_sum_2() == 0
