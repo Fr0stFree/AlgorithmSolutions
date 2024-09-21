78448085d7af7484bcbe0c71db0601e227654265
Danila Krasov <FrostFree@inbox.ru>
2024-09-21 23:15:52 +0300

    solve one more go problem

---
+++ go/digitalRoot.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	fmt.Println(DigitalRoot(942) == 6)
+	fmt.Println(DigitalRoot(132189) == 6)
+
+}
+
+func DigitalRoot(n int) int {
+	digits := strings.Split(strconv.FormatInt(int64(n), 10), "")
+	sum := 0
+	for _, digit := range digits {
+		number, _ := strconv.Atoi(digit)
+		sum += number
+	}
+	if sum > 9 {
+		return DigitalRoot(sum)
+	}
+	return sum
+}
