9d4547a7bf0c3f9c5003156672ad0cddbac41311
Danila Krasov <FrostFree@inbox.ru>
2024-09-23 22:57:45 +0300

    solve one more go problem

--- go/splitStrings.go
+++ go/splitStrings.go
@@ -1,1 +1,28 @@
 package main
+
+import (
+	"fmt"
+	"slices"
+)
+
+func main() {
+	result := Solution("abcdefge")
+	expected := []string{"ab", "cd", "ef"}
+	fmt.Println(slices.Equal(result, expected))
+}
+
+func Solution(str string) []string {
+	runeArray := []rune(str)
+	var result []string
+
+	for index := 0; index < len(runeArray); index += 2 {
+		var nextLetter string
+		if index+1 == len(runeArray) {
+			nextLetter = "_"
+		} else {
+			nextLetter = string(runeArray[index+1])
+		}
+		result = append(result, string(runeArray[index])+nextLetter)
+	}
+	return result
+}
