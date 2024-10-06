5d5413516c0e23c68c5bbc92f0a5d8630e1c94c2
frostfree <me@frostfree.ru>
2024-10-06 13:58:42 +0300

    add binary heap python impl

---
+++ python/binary_heap.py
@@ -0,0 +1,54 @@
+from contextlib import suppress
+from math import inf, isinf
+
+
+class BinHeap[T]:
+    def __init__(self) -> None:
+        self._collection = [None]
+
+    def add(self, elem: T) -> None:
+        print(f"add {elem} to {self._collection}")
+        self._collection.append(elem)
+        self._sift_up(index=len(self._collection)-1)
+
+    def pop(self) -> T:
+        if len(self._collection) == 1:
+            raise IndexError("Heap is empty")
+
+        if len(self._collection) == 2:
+            return self._collection.pop()
+
+        elem = self._collection[1]
+        self._collection[1] = self._collection.pop()
+        self._sift_down(index=1)
+        return elem
+
+    def _sift_down(self, index: int) -> None:
+        elem = self._collection[index]
+        left, right = inf, inf
+        with suppress(IndexError):
+            left = self._collection[index * 2]
+        with suppress(IndexError):
+            right = self._collection[index * 2 + 1]
+
+        if isinf(left) and isinf(right):  # no children. Element on the right place
+            return
+
+        minimum = min(left, right, elem)
+        if minimum == elem:
+            return
+        if minimum == left:
+            self._collection[index], self._collection[index*2] = self._collection[index*2], elem
+            self._sift_down(index=index*2)
+        if minimum == right:
+            self._collection[index], self._collection[index*2+1] = self._collection[index*2+1], elem
+            self._sift_down(index=index*2+1)
+
+    def _sift_up(self, index: int) -> None:
+        elem = self._collection[index]
+        ancestor = self._collection[index // 2]
+        if ancestor is None:  # the elem become the root
+            return
+        if ancestor > elem:
+            self._collection[index // 2], self._collection[index] = self._collection[index], self._collection[index // 2]
+            return self._sift_up(index//2)
