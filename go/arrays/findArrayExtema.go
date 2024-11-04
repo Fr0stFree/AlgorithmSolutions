b5ef0766d8a73219f97328924167a8c953fb7a34
krasov <me@frostfree.ru>
2024-11-04 14:34:13 +0300

    solve one more go problem

---
+++ go/arrays/findArrayExtema.go
@@ -0,0 +1,41 @@
+// Дан массив, состоящий из целых чисел. Напишите программу, которая в данном массиве определит количество элементов,
+// у которых два соседних и, при этом, оба соседних элемента меньше данного.
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	data := readInputData()
+	result := countExtrema(data)
+	fmt.Println(result)
+}
+
+func countExtrema(data []int) int {
+	var counter int
+	for index := 1; index < len(data)-1; index++ {
+		if data[index] > data[index+1] && data[index] > data[index-1] {
+			counter++
+			index++
+		}
+	}
+	return counter
+}
+
+func readInputData() []int {
+	reader := bufio.NewReader(os.Stdin)
+	_, _ = reader.ReadString('\n')
+	text, _ := reader.ReadString('\n')
+	numbers := strings.Split(strings.TrimRight(text, "\n"), " ")
+
+	result := make([]int, len(numbers))
+	for index, elem := range numbers {
+		result[index], _ = strconv.Atoi(elem)
+	}
+	return result
+}
