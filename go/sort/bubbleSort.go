a40938e6460687201d7470cd4ffdd1034976511f
krasov <me@frostfree.ru>
2024-11-12 18:24:56 +0300

    solve one go problem

---
+++ go/sort/bubbleSort.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	values := readInput()
+	bubbleSort(values)
+	fmt.Println(strings.Trim(fmt.Sprint(values), "[]"))
+}
+
+func bubbleSort(values []int) {
+	for idx := range values {
+		for offset := range values[idx:] {
+			if values[idx] > values[idx+offset] {
+				values[idx], values[idx+offset] = values[idx+offset], values[idx]
+			}
+		}
+	}
+}
+
+func readInput() []int {
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Split(bufio.ScanWords)
+	scanner.Scan()
+	size, _ := strconv.Atoi(scanner.Text())
+	values := make([]int, size)
+	for idx := range size {
+		scanner.Scan()
+		values[idx], _ = strconv.Atoi(scanner.Text())
+	}
+	return values
+}
