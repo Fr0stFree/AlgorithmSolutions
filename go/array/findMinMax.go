1227124c5a30fdfe337e94a238e1a0c7209ad6f9
krasov <me@frostfree.ru>
2024-11-13 00:33:15 +0300

    solve one more go problem

--- go/array/findMinMax.go
+++ go/array/findMinMax.go
@@ -3,15 +3,46 @@ package main
 import (
 	"fmt"
 	"sort"
+	"strconv"
+	"bufio"
+	"os"
+	"math"
 )
 
 func main() {
-	data := []int{1, 2, 3, 4, 5}
-	result := MinMax(data)
-	fmt.Println(result)
+	data := readInput()
+	minValue, maxValue := findMinMaxByLinearSearch(data)
+	fmt.Println(minValue)
+	fmt.Println(maxValue)
 }
 
-func MinMax(arr []int) [2]int {
+func findMinMaxBySort(arr []int) (int, int) {
 	sort.Ints(arr)
-	return [2]int{arr[0], arr[len(arr)-1]}
+	return arr[0], arr[len(arr)-1]
+}
+
+func findMinMaxByLinearSearch(arr []int) (int, int) {
+	minValue, maxValue := math.MaxInt, math.MinInt
+	for _, value := range arr {
+		if value > maxValue {
+			maxValue = value
+		}
+		if value < minValue {
+			minValue = value
+		}
+	}
+	return minValue, maxValue
+}
+
+func readInput() []int {
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Split(bufio.ScanWords)
+	scanner.Scan()
+	size, _ := strconv.Atoi(scanner.Text())
+	values := make([]int, size)
+	for idx := range size {
+		scanner.Scan()
+		values[idx], _ = strconv.Atoi(scanner.Text())
+	}
+	return values
 }
