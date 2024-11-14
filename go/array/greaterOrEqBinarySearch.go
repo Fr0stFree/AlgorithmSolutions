c68284e74a46e7b11a84e58d7201b00f9b40b9e7
krasov <me@frostfree.ru>
2024-11-14 14:55:04 +0300

    add one more go solution

---
+++ go/array/greaterOrEqBinarySearch.go
@@ -0,0 +1,48 @@
+// У вас есть вектор целых чисел, который отсортирован в порядке возрастания. Напишите программу, которая принимает
+// целое число N и использует встроенный алгоритм std::lower_bound для определения первого элемента в векторе,
+// который не меньше N
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	values, lookingValue := readInputData()
+	idx := lowerBound(values, lookingValue)
+	fmt.Println(idx)
+}
+
+func lowerBound(array []int, target int) int {
+	low, high, mid := 0, len(array)-1, 0
+	for low <= high {
+		mid = (low + high) / 2
+		if array[mid] >= target {
+			high = mid - 1
+		} else {
+			low = mid + 1
+		}
+	}
+	if low == 0 && array[low] > target {
+		return -1
+	}
+	return low
+}
+
+func readInputData() ([]int, int) {
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Split(bufio.ScanWords)
+	scanner.Scan()
+	size, _ := strconv.Atoi(scanner.Text())
+	values := make([]int, size)
+	for index := range size {
+		scanner.Scan()
+		values[index], _ = strconv.Atoi(scanner.Text())
+	}
+	scanner.Scan()
+	lookingValue, _ := strconv.Atoi(scanner.Text())
+	return values, lookingValue
+}
