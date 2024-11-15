cc89737a9b6bec589b2945662311d93845f13965
krasov <me@frostfree.ru>
2024-11-15 13:57:40 +0300

    solve one more go problem

---
+++ go/sort/stringSort.go
@@ -0,0 +1,46 @@
+// Вам нужно склеить все строчки в одну большую строчку так, чтобы она была лексикографически минимальной.
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	data := readInputData()
+	for _, element := range data {
+		word := makeBiggestWord(element)
+		fmt.Println(word)
+	}
+}
+
+func makeBiggestWord(parts []string) string {
+	sort.Slice(parts, func(i, j int) bool {
+		return parts[i] + parts[j] < parts[j] + parts[i]
+	})
+	return strings.Join(parts, "")
+}
+
+func readInputData() [][]string {
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Split(bufio.ScanWords)
+	
+	scanner.Scan()
+	testAmount, _ := strconv.Atoi(scanner.Text())
+	tests := make([][]string, testAmount)
+	for testIdx := range testAmount {
+		scanner.Scan()
+		size, _ := strconv.Atoi(scanner.Text())
+		words := make([]string, size)
+		for idx := range size {
+			scanner.Scan()
+			words[idx] = scanner.Text()
+		}
+		tests[testIdx] = words
+	}
+	return tests
+}
