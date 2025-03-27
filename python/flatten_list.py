5ee7c27c35622f64f7274ac484a1d38d096bfa46
krasov <me@frostfree.ru>
2025-03-27 15:51:31 +0300

    solve one more python problem

--- python/flatten_list.py
+++ python/flatten_list.py
@@ -16,5 +16,15 @@ def flatten(data: list[int | list]) -> list[int]:
 
     return result
 
+def flatten_recursive(data: list[int | list]) -> list[int]:
+    if not data:
+        return []
+    
+    item = data.pop()
+    if isinstance(item, int):
+        return flatten_recursive(data) + [item]
+    else:
+        return flatten_recursive(data) + flatten_recursive(item)
 
 assert flatten([3, 1, [7, [9, 1]], 4, [7], [[9, [7]], 1, 2], 3, [3, 5, 1, 6]]) == [3, 1, 7, 9, 1, 4, 7, 9, 7, 1, 2, 3, 3, 5, 1, 6]
+assert flatten_recursive([3, 1, [7, [9, 1]], 4, [7], [[9, [7]], 1, 2], 3, [3, 5, 1, 6]]) == [3, 1, 7, 9, 1, 4, 7, 9, 7, 1, 2, 3, 3, 5, 1, 6]
