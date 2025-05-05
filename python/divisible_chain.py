d9439512ebc425610eaeec94c5bbfbe27b2c6027
krasov <me@frostfree.ru>
2025-05-05 23:30:19 +0300

    solve one more python problem

--- python/divisible_chain.py
+++ python/divisible_chain.py
@@ -1,16 +1,32 @@
-def divisible_chain(n: int, k: int) -> str:
-    is_divisible = lambda divisible, divider: divisible % divider == 0
-    result = [n]
-
-    while n != 1:
-        if is_divisible(n, k):
-            n //= k
-        else:
-            n -= 1
-        result.append(n)
-
-    return " -> ".join(map(str, result))
+import math
 
 
-if __name__ == '__main__':
-    assert divisible_chain(29, 3) == '29 -> 28 -> 27 -> 9 -> 3 -> 1'
+def divisible_chain(start: int, divisor: int) -> str:
+    def get_divisors(n: int) -> tuple[int, list[int]]:
+        if n < 1:
+            return int(math.inf), []
+
+        if n == 1:
+            return 1, [n]
+
+        score1, path1 = get_divisors(n - 1)
+        score1 += 1
+
+        if n % divisor == 0:
+            score2, path2 = get_divisors(n // divisor)
+            score2 += 1
+
+            if score2 < score1:
+                path2.append(n)
+                return score2, path2
+
+        path1.append(n)
+        return score1, path1
+
+    _, path = get_divisors(start)
+    return " -> ".join(map(str, reversed(path)))
+
+
+if __name__ == "__main__":
+    print(divisible_chain(29, 3))
+    assert divisible_chain(29, 3) == "29 -> 28 -> 27 -> 9 -> 3 -> 1"
