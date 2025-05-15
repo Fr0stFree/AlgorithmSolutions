2c08666c5a7418e50ce66fdc86d0857620fcd00b
krasov <me@frostfree.ru>
2025-05-15 14:32:42 +0300

    remove old files

--- python/is_palindrome.py
+++ python/is_palindrome.py
@@ -5,16 +5,17 @@ You may assume that each input would have exactly one solution, and you may not 
 You can return the answer in any order.
 """
 
+
 class Solution:
     def isPalindrome(self, x: int) -> bool:
         for index, _ in enumerate((palindrome := str(x))):
-            if palindrome[index] != palindrome[-index-1]:
+            if palindrome[index] != palindrome[-index - 1]:
                 return False
         return True
-    
-    
-if __name__ == '__main__':
+
+
+if __name__ == "__main__":
     solution = Solution()
-    
+
     assert solution.isPalindrome(123) is False
     assert solution.isPalindrome(121) is True
