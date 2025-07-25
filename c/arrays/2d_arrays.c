0863af709a6c9e2ae9486d29431a2cdcf0ecf3bd
krasov <me@frostfree.ru>
2025-07-25 15:32:14 +0300

    solve one more c problem

--- c/arrays/2d_arrays.c
+++ c/arrays/2d_arrays.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
-#define ROWS 3
+#define ROWS 4
 #define COLUMNS 3
 
-void print_array(int array[COLUMNS][ROWS]);
+void print_array(int array[ROWS][COLUMNS]);
 
 int main(void)
 {
     int ar_2D[ROWS][COLUMNS];
-    for (short col_idx = 0; col_idx < COLUMNS; col_idx++)
+    for (short row_idx = 0; row_idx < ROWS; row_idx++)
     {
-        for (short row_idx = 0; row_idx < ROWS; row_idx++)
+        for (short col_idx = 0; col_idx < COLUMNS; col_idx++)
         {
-            scanf("%d", &ar_2D[col_idx][row_idx]);
+            scanf("%d", &ar_2D[row_idx][col_idx]);
         }
     }
 
@@ -19,13 +19,13 @@ int main(void)
     return 0;
 }
 
-void print_array(int array[COLUMNS][ROWS])
+void print_array(int array[ROWS][COLUMNS])
 {
-    for (short col_idx = 0; col_idx < COLUMNS; col_idx++)
+    for (short row_idx = 0; row_idx < COLUMNS; row_idx++)
     {
-        for (short row_idx = 0; row_idx < ROWS; row_idx++)
+        for (short col_idx = 0; col_idx < ROWS; col_idx++)
         {
-            printf("%d ", array[col_idx][row_idx]);
+            (col_idx < ROWS - 1) ? printf("%d ", array[col_idx][row_idx]) : printf("%d", array[col_idx][row_idx]);
         }
         putchar('\n');
     }
