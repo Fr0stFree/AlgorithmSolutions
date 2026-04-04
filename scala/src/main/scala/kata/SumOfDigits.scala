885b129e0e79818443730deccc79c8d30d1b7616
krasov <me@frostfree.ru>
2026-04-04 23:58:29 +0300

    solve one more scala problem

--- scala/src/main/scala/kata/SumOfDigits.scala
+++ scala/src/main/scala/kata/SumOfDigits.scala
@@ -1,5 +1,11 @@
 package kata
 
-object SumOfDigits {
-  def digitalRoot(n: Int): Int = ???
+object SumOfDigits extends App {
+  def digitalRoot(n: Int): Int = {
+    n match
+      case number if number < 10 => number
+      case number => digitalRoot(number.toString.map(_.asDigit).sum)
+  }
+
+  println(digitalRoot(942))
 }
